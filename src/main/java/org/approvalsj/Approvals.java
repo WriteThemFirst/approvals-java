ddb21ebc0e74279e8a85e35de0dd9f46bf14e36a
Michel Daviot <michel@daviot.info>
2018-02-17 15:46:51 +0100

    :recycle: refactored first test for Approvals

--- src/main/java/org/approvalsj/Approvals.java
+++ src/main/java/org/approvalsj/Approvals.java
@@ -1,6 +1,12 @@
 package org.approvalsj;
 
 public class Approvals {
-    public  void verify(String some_text) {
+    private final Class<?> testedClass;
+
+    public Approvals(Class<?> testedClass) {
+        this.testedClass = testedClass;
+    }
+
+    public void verify(String methodName, Object actual) {
     }
 }
--- src/test/java/org/approvalsj/example/ApproveSimpleObject.java
+++ src/test/java/org/approvalsj/example/ApproveSimpleObject.java
@@ -1,30 +1,30 @@
 package org.approvalsj.example;
 
 import org.approvalsj.Approvals;
+import org.approvalsj.util.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedWriter;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.nio.file.Path;
 
 public class ApproveSimpleObject {
-    Approvals approvals = new Approvals();
+    Approvals approvals = new Approvals(getClass());
 
     @Test
     void approvalShouldDoNothingWhenApprovedFileExists() throws Exception {
-        String packageName = getClass().getPackage().getName();
-        String path = "src/test/resources/" + Arrays.stream(packageName.split("\\.")).collect(Collectors.joining("/"));
-        String fileName = "ApproveSimpleObject-approvalShouldDoNothingWhenApprovedFileExists.approved";
+        FileUtils fileUtils = new FileUtils(getClass());
+        String methodName = "approvalShouldDoNothingWhenApprovedFileExists";
+        Path approvedFile = fileUtils.getApprovedFile(methodName);
 
-        Files.createDirectories(Paths.get(path));
-        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(path, fileName))) {
+        Files.createDirectories(approvedFile.getParent());
+        try (BufferedWriter writer = Files.newBufferedWriter(approvedFile)) {
             writer.write("some test");
         }
 
-
-        approvals.verify("some text");
+        approvals.verify(methodName,"some text");
+        Files.delete(approvedFile);
     }
+
 
 }
