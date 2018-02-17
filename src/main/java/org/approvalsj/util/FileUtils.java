138bb90370edf6bc5d16ea2f884bbd63baaa9c1c
Michel Daviot <michel@daviot.info>
2018-02-17 15:29:35 +0100

    :sparkles: FileUtils defines path to accepted file

---
+++ src/main/java/org/approvalsj/util/FileUtils.java
@@ -0,0 +1,26 @@
+package org.approvalsj.util;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtils {
+    private final Class<?> aClass;
+    private final Path folder;
+
+    public FileUtils(Class<?> aClass) {
+        this.aClass = aClass;
+        this.folder = getFolder();
+    }
+
+    public Path getApprovedFile(String methodName) {
+        String fileName = String.format("%s.approved", methodName);
+        return folder.resolve(fileName);
+    }
+
+    private Path getFolder() {
+        String packageName = aClass.getPackage().getName();
+        Path packageResourcesPath = Paths.get("src/test/resources/", packageName.split("\\."));
+        return packageResourcesPath.resolve(aClass.getSimpleName());
+    }
+
+}
---
+++ src/test/java/org/approvalsj/util/FileUtilsTest.java
@@ -0,0 +1,20 @@
+package org.approvalsj.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class FileUtilsTest {
+
+    @Test
+    void approvedFileShouldBeCorrect() {
+        FileUtils fileUtils = new FileUtils(getClass());
+
+        Path approvedFile = fileUtils.getApprovedFile("approvedFileShouldBeCorrect");
+
+        Path expectedPath= Paths.get("src/test/resources/org/approvalsj/util/FileUtilsTest/approvedFileShouldBeCorrect.approved");
+        Assertions.assertEquals(expectedPath, approvedFile);
+    }
+}
