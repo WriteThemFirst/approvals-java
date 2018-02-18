6823af9d9e6a50005a3f12ec4dd995170889d587
Michel Daviot <michel@daviot.info>
2018-02-18 19:31:20 +0100

    :memo: demonstrate how to manually approve with gvim

--- src/test/java/org/demo/simple/ApproveString.java
+++ src/test/java/org/demo/simple/ApproveString.java
@@ -1,10 +1,14 @@
 package org.demo.simple;
 
 import org.approvalsj.Approvals;
+import org.approvalsj.approbation.ExecApprover;
 import org.junit.jupiter.api.Test;
 
 public class ApproveString {
-    Approvals approvals = new Approvals(getClass());
+    Approvals approvals = new Approvals(
+            getClass(),
+            new ExecApprover("cmd /c gvimdiff \"%s\" \"%s\""));
+
 
     @Test
     void verifySimpleString() {
