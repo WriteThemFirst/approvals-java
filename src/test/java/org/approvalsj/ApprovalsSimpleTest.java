65f01abe0066ff374788de91534859a6b57c9d41
Michel Daviot <michel@daviot.info>
2018-02-18 10:35:07 +0100

    :white_check_mark: cleanup after this test

--- src/test/java/org/approvalsj/ApprovalsSimpleTest.java
+++ src/test/java/org/approvalsj/ApprovalsSimpleTest.java
@@ -26,6 +26,7 @@ public class ApprovalsSimpleTest {
                 .hasMessage("expected: <expected text> but was: <actual text>");
 
         fileUtils.removeApproved();
+        fileUtils.removeReceived();
     }
 
     @Test
