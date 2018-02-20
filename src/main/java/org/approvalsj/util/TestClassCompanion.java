90977f7f09812032a379b639c87ca8f004108606
Antoine Neveux <aneveux@protonmail.com>
2018-02-20 13:14:55 +0100

    :fire: Merged StackUtils in TestClassCompanion.

--- src/main/java/org/approvalsj/util/TestClassCompanion.java
+++ src/main/java/org/approvalsj/util/TestClassCompanion.java
@@ -1,66 +1,75 @@
 package org.approvalsj.util;
 
+import static java.lang.String.format;
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.newBufferedWriter;
+import static org.approvalsj.util.FileUtils.silentRead;
+import static org.approvalsj.util.FileUtils.silentRemove;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static java.lang.String.format;
-import static java.nio.file.Files.*;
-import static org.approvalsj.util.FileUtils.*;
-import static org.approvalsj.util.FileUtils.silentRead;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class TestClassCompanion {
     private final Class<?> testClass;
     private final Path folder;
-    private final StackUtils stackUtils;
-
 
     public TestClassCompanion(Class<?> testClass) {
         this.testClass = testClass;
         this.folder = folderForClass();
-        this.stackUtils = new StackUtils(testClass);
     }
+
 
     public void writeApproved(String content) {
         Path approvedFile = approvedFile();
         write(content, approvedFile);
     }
 
+
     public String readApproved() {
         return silentRead(approvedFile());
     }
 
+
     public void removeApproved() {
         silentRemove(approvedFile());
     }
+
 
     public void writeReceived(String content) {
         Path receivedFile = receivedFile();
         write(content, receivedFile);
     }
 
+
     public String readReceived() {
         return silentRead(receivedFile());
     }
+
 
     public void removeReceived() {
         silentRemove(receivedFile());
     }
 
+
     public Path approvedFile() {
-        return approvedFile(stackUtils.methodName().get());
+        return approvedFile(methodName().get());
     }
 
+
     public Path receivedFile() {
-        return receivedFile(stackUtils.methodName().get());
+        return receivedFile(methodName().get());
     }
+
 
     Path receivedFile(String methodName) {
         String fileName = format("%s.received", methodName);
         return folder.resolve(fileName);
     }
+
 
     Path approvedFile(String methodName) {
         String fileName = format("%s.approved", methodName);
@@ -68,21 +77,40 @@ public class TestClassCompanion {
     }
 
 
+    /**
+     * Gets from the stack the name of the method which was called from the class.
+     *
+     * @return a method name, or empty if none was found
+     */
+    public Optional<String> methodName() {
+        return Arrays.stream(Thread.currentThread()
+                                   .getStackTrace())
+                     .filter(e -> e.getClassName()
+                                   .equals(testClass.getName()))
+                     .filter(e -> !e.getMethodName()
+                                    .startsWith("lambda$"))
+                     .map(e -> e.getMethodName())
+                     .findFirst();
+    }
+
+
     private Path folderForClass() {
-        String packageName = testClass.getPackage().getName();
+        String packageName = testClass.getPackage()
+                                      .getName();
         Path packageResourcesPath = Paths.get("src/test/resources/", packageName.split("\\."));
         return packageResourcesPath.resolve(testClass.getSimpleName());
     }
 
+
     private void write(String content, Path file) {
         try {
             createDirectories(file.getParent());
-        } catch (IOException e) {
+        } catch(IOException e) {
             throw new RuntimeException(e);
         }
-        try (BufferedWriter writer = newBufferedWriter(file)) {
+        try(BufferedWriter writer = newBufferedWriter(file)) {
             writer.write(content);
-        } catch (IOException e) {
+        } catch(IOException e) {
             String message = format("Could not write to file %s because of %s", file.toAbsolutePath(), e);
             throw new RuntimeException(message, e);
         }
--- src/test/java/org/approvalsj/util/TestClassCompanionTest.java
+++ src/test/java/org/approvalsj/util/TestClassCompanionTest.java
@@ -1,5 +1,6 @@
 package org.approvalsj.util;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
@@ -8,55 +9,80 @@ import java.nio.file.Paths;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TestClassCompanionTest {
-    private TestClassCompanion testClassCompanion = new TestClassCompanion(getClass());
+    private TestClassCompanion companion = new TestClassCompanion(getClass());
+
 
     @Test
     void approvedFileShouldBeExpectedPath() {
         //WHEN
-        Path approvedFile = testClassCompanion.approvedFile("approvedFileShouldBeCorrect");
+        Path approvedFile = companion.approvedFile("approvedFileShouldBeCorrect");
 
         //THEN
-        Path expectedPath = Paths.get("src/test/resources/org/approvalsj/util/TestClassCompanionTest/approvedFileShouldBeCorrect.approved");
+        Path expectedPath = Paths.get(
+                "src/test/resources/org/approvalsj/util/TestClassCompanionTest/approvedFileShouldBeCorrect.approved");
         assertThat(approvedFile).isEqualTo(expectedPath);
     }
+
 
     @Test
     void approvedFileShouldBeReadAfterWritten() {
         //WHEN
         String content = "some content\non 2 lines";
-        testClassCompanion.writeApproved(content);
+        companion.writeApproved(content);
 
         //THEN
-        String actualContent = testClassCompanion.readApproved();
+        String actualContent = companion.readApproved();
         assertThat(actualContent).isEqualTo(content);
 
         //CLEANUP
-        testClassCompanion.removeApproved();
+        companion.removeApproved();
     }
+
 
     @Test
     void receivedFileShouldBeReadAfterWritten() {
         //WHEN
         String content = "some content\non 2 lines";
-        testClassCompanion.writeReceived(content);
+        companion.writeReceived(content);
 
         //THEN
-        String actualContent = testClassCompanion.readReceived();
+        String actualContent = companion.readReceived();
         assertThat(actualContent).isEqualTo(content);
 
         //CLEANUP
-        testClassCompanion.removeReceived();
+        companion.removeReceived();
     }
+
 
     @Test
     void readApprovedShouldBeNullWhenFileMissing() {
         //GIVEN
-        testClassCompanion.removeApproved();
+        companion.removeApproved();
 
         //WHEN
-        String read = testClassCompanion.readApproved();
+        String read = companion.readApproved();
 
         //THEN
         assertThat(read).isNull();
+    }
+
+
+    @Test
+    void methodNameShouldBeEmpty() {
+        final TestClassCompanion stringCompanion = new TestClassCompanion(String.class);
+        assertThat(stringCompanion.methodName()).isEmpty();
+    }
+
+
+    @Test
+    void methodNameShouldBeTheMethodName() {
+        assertThat(companion.methodName()).contains("methodNameShouldBeTheMethodName");
+    }
+
+
+    @Test
+    void methodNameShouldNotBeLambda() {
+        Stream.of("whatever")
+              .forEach(s -> assertThat(companion.methodName()).contains("methodNameShouldNotBeLambda"));
     }
 }
