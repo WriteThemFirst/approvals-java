ebe46435ab59848f8d18ad538a61ec852acb286a
Antoine Neveux <aneveux@protonmail.com>
2018-02-22 01:06:28 +0100

    :memo: Started ApprovalsFiles javadoc.

--- src/main/java/com/github/writethemfirst/approvals/ApprovalsFiles.java
+++ src/main/java/com/github/writethemfirst/approvals/ApprovalsFiles.java
@@ -13,19 +13,59 @@ import static java.lang.String.format;
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.newBufferedWriter;
 
+/**
+ * # ApprovalsFiles
+ *
+ * *Approval Testing* relies on comparing data produced by a *Program Under Tests* and data which has been reviewed and
+ * validated by the developer. This validated data is stored in files along with the project, and is used for
+ * comparisons with the data produced by the program's execution.
+ *
+ * Two files are needed for computing *Approval Tests*: an *approved* file, which can (and should) be committed with the
+ * source code, containing data validated by the developer, and a *received* file, which temporarly holds the data
+ * produced by the *Program Under Tests* execution. Those files are then compared to validate the proper behavior of the
+ * program.
+ *
+ * That `ApprovalsFiles` class can be seen as a companion to a test class since it provides useful methods allowing to
+ * read and write both *approved* and *received* files. It is linked to a test class since the produced files will be
+ * named after the test class and its methods.
+ *
+ * An `ApprovalsFiles` object will be automatically created and attached to any created `Approvals` object, but you may
+ * choose to instanciate one yourself for accessing and managing the *approved* and *received* files of a particular
+ * class.
+ *
+ * @author mdaviot / aneveux
+ * @version 1.0
+ */
 public class ApprovalsFiles {
+
     private final Class<?> testClass;
     private final Path folder;
 
 
-    public ApprovalsFiles(Class<?> testClass) {
+    /**
+     * Constructs an `ApprovalsFiles` instance.
+     *
+     * @param testClass The test class linked to this intance. Created files will contain that class name in their
+     *                  path.
+     */
+    public ApprovalsFiles(final Class<?> testClass) {
         this.testClass = testClass;
         this.folder = folderForClass();
     }
 
 
-    public void writeApproved(String content) {
-        Path approvedFile = approvedFile();
+    /**
+     * Writes the provided content in the *approved* file linked to the current method execution.
+     *
+     * That method will actually retrieve the method from which the call has been made and name the *approved* file from
+     * the `testClass` attribute, but also the parent method calling this one.
+     *
+     * If the file doesn't exist, that method will create it in the `src/test/resources` folder.
+     *
+     * @param content Content to be written in the *approved* file linked to the parent method execution.
+     */
+    public void writeApproved(final String content) {
+        final Path approvedFile = approvedFile();
         write(content, approvedFile);
     }
 
