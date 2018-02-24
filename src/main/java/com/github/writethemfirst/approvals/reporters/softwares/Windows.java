9f7ae2362bd3b7c5ab13c20679363e249801a33a
Michel Daviot <michel@daviot.info>
2018-02-24 18:56:43 +0100

    :recycle: KDiff3, IntelliJ and Gvim reporters for Windows

--- src/main/java/com/github/writethemfirst/approvals/reporters/softwares/Windows.java
+++ src/main/java/com/github/writethemfirst/approvals/reporters/softwares/Windows.java
@@ -7,19 +7,17 @@ import com.github.writethemfirst.approvals.reporters.commands.Command;
 
 public interface Windows {
 
-    /**
-     * Download GVim from http://www.vim.org/download.php
-     */
-    Reporter GVIM = new WindowsReporter("cmd /c gvimdiff %approved% %received%");
-
-
     Reporter IDEA = new CommandReporter(new Command("%programFiles%\\JetBrains", "idea64.exe"), "merge %approved% %received% %approved%");
 
     /**
      * Download KDiff3 from https://sourceforge.net/projects/kdiff3/files/
      */
     Reporter KDIFF = new CommandReporter(new Command("%programFiles%\\KDiff3", "kdiff3.exe"), "%approved% %received% -o %approved%");
-//    Reporter GVIM = new CommandReporter(new Command("%programFiles%\\Vim", "gvimdiff.exe"), "%approved% %received%");
+
+    /**
+     * Download GVim from http://www.vim.org/download.php
+     */
+    Reporter GVIM = new CommandReporter(new Command("%programFiles%\\Vim", "gvim.exe"), "-d %approved% %received% %received%");
 
 }
 
--- src/test/java/org/demo/simple/ApproveString.java
+++ src/test/java/org/demo/simple/ApproveString.java
@@ -1,19 +1,19 @@
 package org.demo.simple;
 
-import static com.github.writethemfirst.approvals.reporters.softwares.Windows.IDEA;
-
 import com.github.writethemfirst.approvals.Approvals;
 import com.github.writethemfirst.approvals.reporters.JUnit5Reporter;
 import org.junit.jupiter.api.Test;
 
+import static com.github.writethemfirst.approvals.reporters.softwares.Windows.*;
+
 
 class ApproveString {
-    private Approvals approvals = new Approvals(IDEA, new JUnit5Reporter());
+    // You would not use that many approvers, this is just to make testing easy.
+    private Approvals approvals = new Approvals(GVIM, KDIFF, IDEA, new JUnit5Reporter());
 
 
     @Test
-    void verifySimpleString()
-        throws Throwable {
+    void verifySimpleString() throws Throwable {
         approvals.verify("my string");
     }
 }
