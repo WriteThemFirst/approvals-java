1e35f647bcbed1b5cb3a6119ea6aeb526d23a645
Antoine Neveux <antoine.neveux@worldline.com>
2018-02-25 20:08:50 +0100

    :memo: Completed StackUtils Javadoc.

--- src/main/java/com/github/writethemfirst/approvals/utils/StackUtils.java
+++ src/main/java/com/github/writethemfirst/approvals/utils/StackUtils.java
@@ -5,47 +5,73 @@ import java.util.Optional;
 import static java.lang.Thread.currentThread;
 import static java.util.Arrays.stream;
 
+/**
+ * # StackUtils
+ *
+ * Set of methods to be used for easing the parsing of the current Thread stacktrace, allowing to retrieve helpful
+ * information about the classes and methods actually calling the *Approval Tests*.
+ *
+ * Those methods allow to retrieve information to be used for generating the default names of files and folders to be
+ * used by the *Approval Files*.
+ *
+ * @author mdaviot / aneveux
+ * @version 1.0
+ */
 public class StackUtils {
+
     /**
-     * Finds in the stack the caller class which is right before the reference class.
+     * Returns the caller class of the one provided as an argument by searching for it in the current thread
+     * stacktrace.
      *
-     * @param reference a non-null class which is used for comparison
-     * @return the first class in the stack which differs
+     * We consider the caller class to be the really first one found in the current thread stacktrace after finding the
+     * reference one (and which obviously isn't the reference one).
+     *
+     * If the caller class can't be found, a `RuntimeException` will be thrown since it isn't supposed to happen.
+     *
+     * @param referenceClass A non-null class for which we want to find the caller class
+     * @return The caller class of the provided one (ie. the first one found which isn't the reference after finding the
+     * reference)
+     * @throws RuntimeException In case the caller class cannot be found in the current thread stacktrace
      */
-    public static Class<?> callerClass(Class<?> reference) {
+    public static Class<?> callerClass(final Class<?> referenceClass) {
         try {
             //can be rewritten with dropWhile in Java 9
-            String refClass = reference.getName();
+            final String reference = referenceClass.getName();
             boolean inApprovals = false;
-            for (StackTraceElement element : currentThread().getStackTrace()) {
-                String elementClass = element.getClassName();
-                if (inApprovals && !elementClass.equals(refClass)) {
+            for (final StackTraceElement element : currentThread().getStackTrace()) {
+                final String elementClass = element.getClassName();
+                if (inApprovals && !elementClass.equals(reference)) {
                     return Class.forName(elementClass);
                 }
-
-                if (elementClass.equals(refClass)) {
+                if (elementClass.equals(reference)) {
                     inApprovals = true;
                 }
             }
-            throw new RuntimeException("bug : no class found");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("bug", e);
+            throw new RuntimeException("Error: cannot find the caller class of: " + reference);
+        } catch (final ClassNotFoundException e) {
+            throw new RuntimeException("Error: cannot find the caller class of: " + referenceClass, e);
         }
     }
 
     /**
-     * Gets from the stack the name of the method which was called from the class.
+     * Returns the caller method of the provided `referenceClass`.
      *
-     * @return a method name, or empty if none was found
+     * We consider the caller method to be the first method from the `referenceClass` called in the current thread
+     * stacktrace, which isn't a lambda.
+     *
+     * If found in the current thread stacktrace, the method name will be returned, wrapped in an `Optional`. If no
+     * method can be found, an empty `Optional` will be returned.
+     *
+     * @param referenceClass The `referenceClass` for which we want to search the caller method in the current thread
+     *                       stacktrace
+     * @return An `Optional` object contaning either the caller method name (as a `String`) or an empty value if it
+     * cannot be found
      */
-    public static Optional<String> callerMethod(Class<?> testClass) {
-        return stream(currentThread()
-            .getStackTrace())
-            .filter(e -> e.getClassName()
-                .equals(testClass.getName()))
-            .filter(e -> !e.getMethodName()
-                .startsWith("lambda$"))
-            .map(e -> e.getMethodName())
+    public static Optional<String> callerMethod(final Class<?> referenceClass) {
+        return stream(currentThread().getStackTrace())
+            .filter(e -> e.getClassName().equals(referenceClass.getName()))
+            .filter(e -> !e.getMethodName().startsWith("lambda$"))
+            .map(StackTraceElement::getMethodName)
             .findFirst();
     }
 }
