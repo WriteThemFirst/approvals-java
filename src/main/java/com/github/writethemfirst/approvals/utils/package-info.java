6886bd245bd290cec4ff8372f2dff419a5c1c09a
Antoine Neveux <antoine.neveux@worldline.com>
2018-02-25 19:18:23 +0100

    :memo: Added some package-info documentation for Utils.

---
+++ src/main/java/com/github/writethemfirst/approvals/utils/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * That package contains *Utils* classes to be used internally by the framework.
+ *
+ * Classes inside that folder aren't meant to be used by the end-user.
+ */
+package com.github.writethemfirst.approvals.utils;
