2fab9ca1e6b2127a00f622858ee647e95cf04fd7
Antoine Neveux <antoine.neveux@worldline.com>
2018-02-25 18:50:13 +0100

    :memo: Adding documentation over the main package.

---
+++ src/main/java/com/github/writethemfirst/approvals/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * That package contains all the root objects to be used while working with *Approvals-Java*.
+ *
+ * Those objects are the main ones to be manipulated by developers, and should allow to directly trigger actions to be
+ * performed by the framework.
+ *
+ * All internal related classes, or classes to be used indirectly by the developers are to be stored in children
+ * packages.
+ */
+package com.github.writethemfirst.approvals;
