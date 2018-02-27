c8170e4d63ee8da9e94affc7f2da781ee358e690
Michel Daviot <michel@daviot.info>
2018-02-27 21:44:02 +0100

    :recycle: Refactored StackUtils.callerClass().

--- src/main/java/com/github/writethemfirst/approvals/utils/StackUtils.java
+++ src/main/java/com/github/writethemfirst/approvals/utils/StackUtils.java
@@ -1,6 +1,9 @@
 package com.github.writethemfirst.approvals.utils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.lang.Thread.currentThread;
 import static java.util.Arrays.stream;
@@ -25,31 +28,27 @@ public class StackUtils {
      * We consider the caller class to be the first one found in the current thread stacktrace after finding the
      * reference one.
      *
-     * If the caller class can't be found, a `RuntimeException` will be thrown since it isn't supposed to happen.
-     *
      * @param referenceClass A non-null class for which we want to find the caller class
      * @return The caller class of the provided one (ie. the first one found which isn't the reference after finding the
      * reference)
-     * @throws RuntimeException In case the caller class cannot be found in the current thread stacktrace
      */
     public static Class<?> callerClass(final Class<?> referenceClass) {
+        //can be rewritten with dropWhile in Java 9
+        final List<String> classesInStack = distinctClassesInStack();
+        final int referenceIndex = classesInStack.indexOf(referenceClass.getName());
+        final String callerClassName = classesInStack.get(referenceIndex + 1);
         try {
-            //can be rewritten with dropWhile in Java 9
-            final String reference = referenceClass.getName();
-            boolean inApprovals = false;
-            for (final StackTraceElement element : currentThread().getStackTrace()) {
-                final String elementClass = element.getClassName();
-                if (inApprovals && !elementClass.equals(reference)) {
-                    return Class.forName(elementClass);
-                }
-                if (elementClass.equals(reference)) {
-                    inApprovals = true;
-                }
-            }
-            throw new RuntimeException("Error: cannot find the caller class of: " + reference);
+            return Class.forName(callerClassName);
         } catch (final ClassNotFoundException e) {
-            throw new RuntimeException("Error: cannot find the caller class of: " + referenceClass, e);
+            throw new Error("Unexpected exception", e);
         }
+    }
+
+    private static List<String> distinctClassesInStack() {
+        return Arrays.stream(currentThread().getStackTrace())
+            .map(e -> e.getClassName())
+            .distinct()
+            .collect(Collectors.toList());
     }
 
     /**
@@ -67,8 +66,9 @@ public class StackUtils {
      * cannot be found
      */
     public static Optional<String> callerMethod(final Class<?> referenceClass) {
+        String referenceClassName = referenceClass.getName();
         return stream(currentThread().getStackTrace())
-            .filter(e -> e.getClassName().equals(referenceClass.getName()))
+            .filter(e -> e.getClassName().equals(referenceClassName))
             .filter(e -> !e.getMethodName().startsWith("lambda$"))
             .map(StackTraceElement::getMethodName)
             .findFirst();
