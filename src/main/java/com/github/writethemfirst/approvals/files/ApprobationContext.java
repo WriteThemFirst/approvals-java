c3bd8348005f1721e93e82246418b20e68ece82f
Michel Daviot <michel@daviot.info>
2018-03-15 21:10:18 +0100

    :page_facing_up: Add missing licence header + missing approved files for test

--- src/main/java/com/github/writethemfirst/approvals/files/ApprobationContext.java
+++ src/main/java/com/github/writethemfirst/approvals/files/ApprobationContext.java
@@ -1,3 +1,20 @@
+/*
+ * Approvals-Java - Approval testing library for Java. Alleviates the burden of hand-writing assertions.
+ * Copyright © 2018 Write Them First!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.writethemfirst.approvals.files;
 
 import java.nio.file.Path;
