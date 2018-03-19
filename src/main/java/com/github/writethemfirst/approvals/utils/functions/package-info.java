09d3d39124305efc3e5dd0f8b3a78352cd30a027
Antoine Neveux <aneveux@protonmail.com>
2018-03-20 00:40:41 +0100

    :bulb: Adding package-info for the functions package.

---
+++ src/main/java/com/github/writethemfirst/approvals/utils/functions/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Approvals-Java - Approval testing library for Java. Alleviates the burden of hand-writing assertions.
+ * Copyright © 2018 Write Them First!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * # Functions Package
+ *
+ * That package contains interfaces representing functions in Java. Those `FunctionX` interfaces can be used as
+ * shortcuts in order to declare and use functions with X parameters. It is way easier to remember and use than using
+ * objects available natively in Java such as `Callable`, `BiFunction` and such.
+ *
+ * That package should only contain interfaces easing the usage of functions and lambdas within the framework.
+ */
+package com.github.writethemfirst.approvals.utils.functions;
