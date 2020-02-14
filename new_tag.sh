e15eb17868fe4d3583703b410035ce3b17a3c197
Michel Daviot <michel@daviot.info>
2020-02-14 15:38:33 +0100

    :bookmark: release v0.13.0

--- new_tag.sh
+++ new_tag.sh
@@ -3,7 +3,7 @@
 NEW_TAG=$1
 LAST_TAG=$(git tag --sort=creatordate | tail -1 | sed 's/v//')
 
-find . -name "*.md" -exec sed -i '' s/$LAST_TAG/$NEW_TAG/g  {} \;
+find . \( -name '*.md' -o -name 'pom.xml' \)  -exec sed -i '' s/$LAST_TAG/$NEW_TAG/g  {} \; -print
 git add .
 git commit -m ":bookmark: release v$NEW_TAG"
-git tag $NEW_TAG
+git tag v$NEW_TAG
