500cecde812515cdc507686410b1d6f1560919f3
Michel Daviot <michel@daviot.info>
2020-06-29 18:16:54 +0200

    :wrench: fix tags

--- new_tag.sh
+++ new_tag.sh
@@ -5,5 +5,5 @@ LAST_TAG=$(git tag --sort=creatordate | tail -1 | sed 's/v//')
 
 find . \( -name '*.md' -o -name 'pom.xml' \)  -exec sed -i '' s/$LAST_TAG/$NEW_TAG/g  {} \; -print
 git add .
-git commit -m ":bookmark: release v$NEW_TAG"
+git commit -m ":bookmark: release $NEW_TAG"
 git tag $NEW_TAG
