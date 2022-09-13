33abf36632e26e50e2f51190af4b90ebeea7e65a
Michel Daviot <michel.daviot@colisweb.com>
2022-09-13 11:53:23 +0200

    :bookmark: release

--- new_tag.sh
+++ new_tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NEW_TAG=$1
+NEW_TAG=${1:-$(gum input --placeholder v1.2.3)}
 LAST_TAG=$(git tag --sort=creatordate | tail -1 | sed 's/v//')
 
 find . \( -name '*.md' -o -name 'pom.xml' \)  -exec sed -i '' s/$LAST_TAG/$NEW_TAG/g  {} \; -print
